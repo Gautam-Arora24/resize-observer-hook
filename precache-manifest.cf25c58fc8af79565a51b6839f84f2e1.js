ae0c7c267bc55398492e83e5d204c4591fa3fa36
Gautam Arora <gautamarora6248@gmail.com>
2021-08-30 16:03:30 +0530

    Updates

---
+++ precache-manifest.cf25c58fc8af79565a51b6839f84f2e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ea190659f63c0b6e9d347ccec19450e",
+    "url": "/resize-observer-hook/index.html"
+  },
+  {
+    "revision": "edbdf813f7fb534602a7",
+    "url": "/resize-observer-hook/static/css/main.76e8236d.chunk.css"
+  },
+  {
+    "revision": "9c951ff8ad7da029981f",
+    "url": "/resize-observer-hook/static/js/2.c6939a77.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/resize-observer-hook/static/js/2.c6939a77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edbdf813f7fb534602a7",
+    "url": "/resize-observer-hook/static/js/main.fbdc9ccf.chunk.js"
+  },
+  {
+    "revision": "9adf0033347a348ede66",
+    "url": "/resize-observer-hook/static/js/runtime-main.0053b0c8.js"
+  }
+]);
