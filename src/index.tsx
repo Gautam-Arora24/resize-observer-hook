327b68b8db443dd7a011708942d365a49df76708
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-09-28 12:48:27 +0530

    migration: Migrate to Typescript.
    Fixes #20.

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect, useRef } from 'react';
 
 const useResizeObserver = (option = 'contentRect') => {
   const ref = useRef(null)
